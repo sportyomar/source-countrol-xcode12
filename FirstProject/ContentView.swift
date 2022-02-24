191a141b55cb0a53e5dfe30417125d35fd26b6e7
Omar Morrison <omar.morrison@sportyventures.com>
2022-02-24 14:59:37 -0500

    added Xcode 12 text

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if isTextShowing {
-                Text("Hello, world!")
+                Text("Hello, world! XCode 12 Branch")
                     .padding()
                     .font(.title)
             } else {
