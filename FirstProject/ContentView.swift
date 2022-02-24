ab0ef66f689ca9371fd76476fd91a55b9c36c336
Omar Morrison <omar.morrison@sportyventures.com>
2022-02-24 14:15:12 -0500

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by sporty on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTextShowing = true
+    var body: some View {
+        VStack {
+            if isTextShowing {
+                Text("Hello, world!")
+                    .padding()
+            } else {
+                Text("")
+                    .padding()
+            }
+            Button(action: {isTextShowing.toggle()}) {
+                Text("Button")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by sporty on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
